4dd2ce74cf5577e298f8c65207920ccb1e1e11e4
alex <workfast@bk.ru>
2020-01-07 02:49:11 +0300

    leak destroyed

--- vm/srcs/vm.c
+++ vm/srcs/vm.c
@@ -59,6 +59,25 @@ static void		print_players(t_arrplayers *players)
 			pl->player_id, pl->prog_size, pl->name, pl->comment);
 }
 
+void			destroy_all(t_vm *vm)
+{
+	t_arrplayers	*players;
+	t_process		*pr;
+	t_process		*prnext;
+
+	players = &vm->players;
+	ft_array_delete_all(&players, free);
+	pr = vm->processes_root;
+	vm->processes_root = NULL;
+	while (pr)
+	{
+		prnext = pr->next;
+		ft_memdel((void **)&pr);
+		pr = prnext;
+	}
+	sdl_destroy(&vm->vis);
+}
+
 int				main(int ac, char *av[])
 {
 	t_vm		vm;
@@ -72,7 +91,7 @@ int				main(int ac, char *av[])
 		ft_printf("Introducing contestants...\n");
 		print_players(&vm.players);
 		infinite_loop(&vm);
-		sdl_destroy(&vm.vis);
 	}
+	destroy_all(&vm);
 	return (0);
 }
